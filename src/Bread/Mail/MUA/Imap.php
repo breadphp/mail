bd176af7823aa7ac252fa156fe2291502b953705
tomasell <alex.tomasello@gmail.com>
2013-12-11 14:45:38 +0100

    Update imap

--- src/Bread/Mail/MUA/Imap.php
+++ src/Bread/Mail/MUA/Imap.php
@@ -1,148 +1,68 @@
 <?php
 namespace Bread\Mail\MUA;
 
-use Bread\Mail\Message;
-use Bread\Types\DateTime;
 use Bread\Helpers\HTML\Page;
+use Bread\Mail\Message;
+use Bread\Media\File\Model as File;
+use Bread\Types\DateTime;
+use ezcMailImapTransport;
+use ezcMailParser;
+use ezcMailText;
+use ezcMailFile;
 
 class Imap
 {
-    public static function getMail($server, $username, $password, $params = array('ALL' => true), $mailbox = 'INBOX', $flag = 'novalidate-cert')
-    {
-        if (! $mailbox = imap_open("{{$server}/{$flag}}$mailbox", $username, $password, 0, 0, array('DISABLE_AUTHENTICATOR' => 'GSSAPI'))) {
-            return array();
-        }
-        return static::search($mailbox, static::normalizeSearch($params));
-    }
 
-    protected static function search($mailbox, $search)
+    public static function fetchMail($server, $username, $password, $mailbox = 'INBOX', $flag = 'UNSEEN')
     {
-        $mail = array();
-        $search = imap_search($mailbox, $search) ? imap_search($mailbox, $search) : array();
-        foreach ($search as $number) {
-            $message = new Message();
-            $headers = imap_headerinfo($mailbox, $number);
-            $message->body = static::getBody($mailbox, $number);
-            foreach (get_object_vars($headers) as $attribute => $value) {
-                switch ($attribute) {
-                    case 'date':
-                        $message->$attribute = new DateTime($value);
-                        break;
-                    case 'subject':
-                        $message->$attribute = $value;
-                        break;
-                    case 'from':
-                        $message->from($headers->from[0]->mailbox . '@' . $headers->from[0]->host, isset($headers->from[0]->personal) ? $headers->from[0]->personal : '');
-                        break;
-                    case 'to':
-                        foreach ($value as $to) {
-                            $message->addTo($headers->to[0]->mailbox . '@' . $headers->to[0]->host, isset($headers->to[0]->personal) ? $headers->from[0]->personal : '');
-                        }
-                        break;
-                    case 'cc':
-                        foreach ($value as $cc) {
-                            $message->addCc($headers->cc[0]->mailbox . '@' . $headers->cc[0]->host, isset($headers->cc[0]->personal) ? $headers->from[0]->personal : '');
-                        }
-                        break;
+        $imap = new ezcMailImapTransport($server);
+        $imap->authenticate($username, $password);
+        $imap->selectMailbox($mailbox);
+        $set = $imap->fetchByFlag($flag);
+        $parser = new ezcMailParser();
+        $messages = $parser->parseMail($set);
+        $mailFetch = array();
+        foreach ($messages as $message) {
+            $mail = new Message();
+            $mail->from($message->from->email,$message->from->name);
+            $mail->subject = $message->subject;
+            $mail->type = Message::TEXT_PLAIN;
+            foreach ($message->to as $to) {
+                $mail->addTo($to->email, $to->name);
+            }
+            foreach ($message->cc as $cc) {
+                $mail->addCc($cc->email, $cc->name);
+            }
+            foreach ($message->fetchParts() as $part) {
+                if ($part instanceof ezcMailText) {
+                    $body = $part->generateBody();
+                    if ($part->subType === 'html') {
+                        $page = new Page(html_entity_decode($body));
+                        $page->query('blockquote')->remove();
+                        $body = htmlspecialchars_decode(strip_tags((string) $page));
+                        $mail->type = Message::TEXT_HTML;
+                    }
+                    $body = preg_replace("/(---BEST-body-BEGIN---.*---BEST-body-END---)?/ms", '', $body);
+                    $body = preg_replace("/(On [\d\/]* [\d:]*,.*wrote:.*)$/m", '', $body);
+                    $body = preg_replace("/(Il giorno [\d]* [\w]* [\d]* [\d:]*,.*scritto:.*)$/ms", '', $body);
+                    $body = preg_replace("/(^>.*(\n|$))+/mi", '', $body);
+                    $body = preg_replace("/(-+Original body-+\W{1})|(-+Messaggio originale-+)/", '', $body);
+                    $body = preg_replace("/(From|Da)(: .*\W{1})/", '', $body);
+                    $body = preg_replace("/(Sent|Data)(: .*\W{1})/", '', $body);
+                    $body = preg_replace("/(To|A)(: .*\W{1})/", '', $body);
+                    $body = preg_replace("/(Cc: .*\W{1})/", '', $body);
+                    $body = trim(preg_replace("/(Subject|Ogg|Oggetto)(: .*)($|\W{2,})/", '', $body));
+                    $mail->body = $body;
+                } elseif ($part instanceof ezcMailFile) {
+                    $file = new File();
+                    $file->name = $part->contentDisposition->fileName;
+                    $file->data = fopen($part->fileName, 'r');
+                    $file->type = "{$part->contentType}/{$part->mimeType}";
+                    $mail->addAttachment($file);
                 }
             }
-            $mail[] = $message;
+            $mailFetch[] = $mail;
         }
-        imap_close($mailbox);
-        return $mail;
-    }
-
-    protected static function normalizeSearch($params)
-    {
-        $conditions = array();
-        foreach ($params as $criteria => $value) {
-            switch (strtoupper($criteria)) {
-                case 'BEFORE':
-                case 'SINCE':
-                case 'ON':
-                    $value = $value->format("d-M-Y");
-                case 'BODY':
-                case 'KEYWORD':
-                case 'SUBJECT':
-                case 'TEXT':
-                case 'UNKEYWORD':
-                    $conditions[] = $criteria . " \"{$value}\"";
-                    break;
-                case 'BCC':
-                case 'CC':
-                case 'FROM':
-                case 'TO':
-                    $value = array_map(function ($address) use($criteria) {
-                        return $criteria . " \"{$address}\"";
-                    }, (array) $value);
-                    $conditions[] = implode(' ', $value);
-                    break;
-                default:
-                    $conditions[] = $criteria;
-            }
-        }
-        return implode(' ', $conditions);
-    }
-
-    protected static function getBody($mailbox, $number)
-    {
-        $structure = imap_fetchstructure($mailbox, $number);
-        $encoding = $structure->encoding;
-        switch ($structure->subtype) {
-            case 'ALTERNATIVE':
-                if($structure->parts[0]->subtype === 'PLAIN') {
-                    $message = imap_fetchbody($mailbox, $number, '1');
-                } else {
-                    $message = imap_fetchbody($mailbox, $number, '1');
-                    $encoding = $structure->parts[0]->encoding;
-                    $page = new Page($message);
-                    $page->query('blockquote')->remove();
-                    $message = strip_tags(htmlspecialchars_decode((string) $page));
-                }
-                break;
-            case 'HTML':
-                $page = new Page(imap_body($mailbox, $number));
-                $page->query('blockquote')->remove();
-                $message = htmlspecialchars_decode((string) $page);
-                break;
-            case 'PLAIN':
-                $message = imap_body($mailbox, $number);
-                break;
-            case 'MIXED':
-//                 foreach ($structure->parts as $i=>$part) {
-//                     switch ($part->)
-//                 }
-        }
-        $message = static::decodeBody($message, $structure->encoding);
-        $message = preg_replace("/(---BEST-MESSAGE-BEGIN---.*---BEST-MESSAGE-END---)?/ms", '', $message);
-        $message = preg_replace("/(On [\d\/]* [\d:]*,.*wrote:.*)$/m", '', $message);
-        $message = preg_replace("/(Il giorno [\d]* [\w]* [\d]* [\d:]*,.*scritto:.*)$/ms", '', $message);
-        $message = trim(preg_replace("/(^>.*(\n|$))+/mi", '', $message));
-        $message = trim(preg_replace("/(-+Original Message-+\W{1})|(-+Messaggio originale-+)/", '', $message));
-        $message = trim(preg_replace("/(From|Da)(: .*\W{1})/", '', $message));
-        $message = trim(preg_replace("/(Sent|Data)(: .*\W{1})/", '', $message));
-        $message = trim(preg_replace("/(To|A)(: .*\W{1})/", '', $message));
-        $message = trim(preg_replace("/(Cc: .*\W{1})/", '', $message));
-        $message = trim(preg_replace("/(Subject|Ogg|Oggetto)(: .*)($|\W{2,})/", '', $message));
-        return $message;
-    }
-
-    /**
-     * 0	7BIT
-     * 1	8BIT
-     * 2	BINARY
-     * 3	BASE64
-     * 4	QUOTED-PRINTABLE
-     * 5	OTHER
-     */
-    protected static function decodeBody($body, $encoding)
-    {
-        if ($encoding === 3) {
-            return base64_decode($body);
-        } elseif ($encoding === 4) {
-            return quoted_printable_decode($body);
-        } else {
-            return $body;
-        }
+        return $mailFetch;
     }
 }
